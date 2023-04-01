215cc78bc44707742c98a356269a638c11dec202
Mahir-Neema <mohi10503@gmail.com>
2023-04-01 15:44:25 +0530

    started expenseslist

--- src/components/Expenseslist.tsx
+++ src/components/Expenseslist.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {Text,FormControl,FormLabel,Input,FormHelperText, Select, Box, Flex, Button, Alert, AlertIcon,} from '@chakra-ui/react'
+import axios from 'axios';
 
 type Props = {}
 
 function Expenseslist({}: Props) {
+  const [username,setusername] = useState('');
+  const [users,setusers] = useState(["Testuser 16:25"]);
+  const [expenses,setexpenses] = useState([]);
+
+  useEffect(()=>{
+      axios.get('http://localhost:5000/users').then((res)=>{
+        if(res.data.length>0){
+          setusers(res.data.map((user:{},index:number)=> res.data[index].username));
+          setusername(res.data[0].username);
+        }
+      }).catch(()=> console.log("error occured at fetching users"));
+  },[])
+
+  const getuserdata = (event: React.ChangeEvent<HTMLInputElement>) =>{
+      setusername(event.target.value);
+  }
+
   return (
-    <div>Expenseslist</div>
+    <div>
+        <div style={{margin:"auto",width:"50%"}}>
+        <FormControl isRequired mt="3" mb="3">
+            <FormLabel >UserName:</FormLabel>
+                <Select placeholder='select user' onChange={getuserdata} size='sm'>
+                  <>
+                    {users.map((users)=>{
+                      return <option key="index" value={users}>{users}</option>
+                    })}
+                  </>
+            </Select>
+            {username ?<FormHelperText>{username} user selected</FormHelperText>:<FormHelperText>Please select the user</FormHelperText>}
+        </FormControl>
+        </div>
+    </div>
   )
 }
 
